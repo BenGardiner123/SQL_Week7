8f117ea12bc3ad71813fe790e8c1bc70c946c5db
BenGardiner123 <102151272@student.swin.edu.au>
2020-04-28 14:38:22 +1000

    task 4 complete i think - may be prob with keys

--- Week7_102151272.sql
+++ Week7_102151272.sql
@@ -1,18 +1,5 @@
-delete from worker;
-
-delete from project;
-
-go
-
-INSERT INTO PROJECT (ProjCode, ProjectTitle) VALUES 
-('1', 'Project One'),
-('2', 'Project Two'),
-('3', 'Project Three');
-
-INSERT INTO WORKER (Wid, Wname, Gender, ProjCode) VALUES 
-('21', 'Dave Jones', 'M', '2'), 
-('22', 'Emma Quilt', 'F', '2'),
-('23', 'Fred Gingers', 'M', '1'),
-('24', 'Pat Smith', 'F', '2'),
-('40', 'Ben Gardiner', 'M', '3'),
-('41', 'Joel Anderson', 'M', '3');
+select table_name from information_schema.tables;
+exec sp_columns project;
+select * from project;
+exec sp_columns worker;
+select * from worker;
